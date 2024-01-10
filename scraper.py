c1f93515156fd565c026e044ae32795417f48123
Piotr Brudny <pbrudny@gmail.com>
2024-01-10 13:04:49 +0100

    Add scraper

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the site to be scraped
+url = 'https://ox.pl'
+
+# Send a HTTP request to the URL
+response = requests.get(url)
+
+# Parse the HTML content of the page
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find elements containing the information you want to extract
+# For example, article titles in <h2> tags
+for title in soup.find_all('h2'):
+    print(title.text.strip())
